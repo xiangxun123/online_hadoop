7359786e6d2777da516e23bae483a2e53424409a
xiangxun123 <36273929+xiangxun123@users.noreply.github.com>
2018-05-01 20:30:01 +0800

    提交测试项目文件
    提交测试项目文件

---
+++ eureka-server/src/main/java/com/eureka/server/Application.java
@@ -0,0 +1,16 @@
+package com.eureka.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Administrator on 2018/2/14.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
